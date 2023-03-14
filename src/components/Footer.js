41d508fa7147c624e8cf4bc7100ebb3d3002a79b
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-03-13 23:39:53 -0300

    footer rename

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Conversor de cores por Felipe Fardo &copy; 2023</p>
+      <p>Conversor de espaço de cores por Felipe Fardo &copy; 2023</p>
     </footer>
   );
 };
