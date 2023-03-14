504422ed0b6de81d1efd94e2c3669df6c1f02214
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-03-13 23:37:42 -0300

    correction rename

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <a href="https://github.com/FelipeFardo" target='_blank' rel="noreferrer">Github de Felipe Fardo</a>
+        <a href="https://github.com/FelipeFardo" target='_blank' rel="noreferrer">GitHub de Felipe Fardo</a>
       <ul className={styles.links_list}>
       <li>
           <NavLink
